80c6aee78001f4477362db3947025d835c191a95
Jonah Andersson <14919667+jonahandersson@users.noreply.github.com>
2021-01-14 16:52:38 +0100

    Update and rename createAzureVM.ps1 to createAzureVMWIndowsDataCenter2019.ps1

--- createAzureVMWIndowsDataCenter2019.ps1
+++ createAzureVMWIndowsDataCenter2019.ps1
@@ -8,27 +8,28 @@ Set-AzContext -SubscriptionName '<<Name of Azure Subscription Account>>'
 New-AzResourceGroup -Name "rg-azurevm-demo-powershell" -Location "NorthEU" 
 
 #Create a credential to use in the VM creation
-#NOTE: Please change the username, password with security in mind 
-$username = 'azurevmadmin' 
-$password = ConvertTo-SecureString 'pa$$word123$%^&*' -AsPlainText -Force
+#NOTE: Please change the username, password with security in mind
+#RECOMMENDED to use Azure Key Vault! 
+$username = '<Your Desired VM admin Name here>' 
+$password = ConvertTo-SecureString '<Your Secure Password Here>' -AsPlainText -Force
 $AzVMWindowsCredentials = New-Object System.Management.Automation.PSCredential ($username, $password)
 
 #Create a Windows Virtual Machine based on desired VM Image, ex. Windows Server 2019 Datacenter 
 New-AzVM 
-    -ResourceGroupName 'psdemo-rg' `
-    -Name 'azvmdemo-powershell-w2019dc' `
-    -Image 'Win2019Datacenter' `
-    -Credential $AzVMWindowsCredentials `
+    -ResourceGroupName '<Your Resource Group Name here>' #example rg-azurevm-demo-powershell
+    -Name 'azvmdemo-powershell-w2019dc' 
+    -Image 'Win2019Datacenter' 
+    -Credential $AzVMWindowsCredentials 
     -OpenPorts 3389
 
 
 #Get the Public IP Address to be able to remote access it via internet using RDP
-Get-AzPublicIpAddress `
-    -ResourceGroupName 'rg-azurevm-demo-powershell' `
+Get-AzPublicIpAddress 
+    -ResourceGroupName '<Your Resource Group Name here>' #example rg-azurevm-demo-powershell
     -Name 'azvmdemo-powershell-w2019dc' | Select-Object IpAddress
 
-#TODO: Remotely access new Azure VM using Remote Desktop via RDP Client
-#NOTE Login using Public IP address and the admin user credetials 
+#TODO: Remotely access new Azure VM using Remote Desktop via RDP Client from your computer
+#NOTE Login using Public IP address and the admin user credentials 
 
 
 #Clean up after this demo
